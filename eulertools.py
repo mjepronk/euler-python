e2f93e700c6299bbad878ffede8f4103dda97902
Matthias Pronk <git@masida.nl>
2011-10-11 15:58:00 +0000

    problem 26 solved
    git-svn-id: http://masida.xs4all.nl/svn/euler/trunk@10 a068edb5-9030-4637-94c8-d4187c712847

--- eulertools.py
+++ eulertools.py
@@ -1,12 +1,61 @@
 # vim: sw=4:ts=4:et:ai
 from __future__ import division
 from math import sqrt, ceil, factorial
+from fractions import gcd
 
-def gcd(a, b):
-    """ Calculate the greatest common divisor """
-    while b > 0:
-        a, b = b, a % b
-    return a
+def multi_order(a, m):
+    """
+    Calculate the multiplicative order
+
+    Source: http://rosettacode.org/wiki/Multiplicative_order
+
+    >>> multi_order(37, 1000)
+    100
+    >>> multi_order(4, 7)
+    3
+    """
+    def multi_order1(a, (p, e)):
+        m = p**e
+        # Calculate Phi(p**e) where p prime
+        t = (p-1)*(p**(e-1))
+        qs = [1,]
+        for f in prime_factors_with_e(t).iteritems():
+            qs = [q * f[0]**j for j in xrange(1+f[1]) for q in qs]
+        qs.sort()
+     
+        for q in qs:
+            if pow(a, q, m) == 1: break
+        return q
+
+    assert gcd(a, m) == 1
+    mofs = (multi_order1(a, r) for r in prime_factors_with_e(m).iteritems())
+    return reduce(lcm, mofs, 1)
+
+def period_len_fraction(p, q):
+    """
+    Calculate the period (repeating decimals) length for a specific fraction
+
+    Sources:
+    http://en.wikipedia.org/wiki/Repeating_decimal
+    http://www.lrz.de/~hr/numb/period.html
+
+	p = numerator
+	q = denominator
+
+    >>> period_len_fraction(1, 7)
+    6
+    >>> period_len_fraction(1, 17)
+    16
+    >>> period_len_fraction(1, 97)
+    96
+    """
+    assert p == 1, "Sorry only 1 allowed for numerator."
+    if not (gcd(p, q) == 1 and gcd(10, q) == 1):
+        # A fraction in lowest terms with a prime denominator other than 2 or 5
+        # (i.e. coprime to 10) always produces a repeating decimal,
+        # thus if this is not the case return 0
+        return 0
+    return multi_order(10, q)
 
 def lcm(a, b):
     """ Calculate the least common multiple """
@@ -14,6 +63,8 @@ def lcm(a, b):
 
 def fibonacci(n=4000000):
     """
+    Generator for the Fibonacci sequence
+
     >>> list(fibonacci(150))
     [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
     """
@@ -59,10 +110,9 @@ def factors(n):
     l = []
     for i in xrange(1, top+1):
         if n % i  == 0:
-            l.append(i)
+            if not (i == top and top*top == n):
+                l.append(i)
             yield i
-    if top * top == n:
-        l = l[:-1]
     for i in reversed(l):
         yield n // i
 
@@ -122,6 +172,10 @@ def prime_factors2(n):
 
 def prime_factors_with_e(n):
     """
+    Factorize n in prime factors with their exponent.
+    Returns a dictionary with the prime factors as keys,
+    and their exponents as values.
+
     >>> prime_factors_with_e(36)
     {2: 2, 3: 2}
     """
@@ -140,6 +194,18 @@ def prime_factors_with_e(n):
             primes = primes_upto(n)
             n = n // test_prime
     return factor_dict
+
+def prime_factors_with_e2(a):
+    for p in primes():
+        j = 0
+        while a%p == 0:
+            a /= p
+            j += 1
+        if j > 0:
+            yield (p,j)
+        if a < p*p: break
+    if a > 1:
+        yield (a,1)
 
 def is_prime(n):
     """
---
+++ problem26.py
@@ -0,0 +1,19 @@
+# vim: sw=4:ts=4:et:ai
+
+from eulertools import period_len_fraction, primes_upto
+
+def main():
+    def tup_max(a, b):
+        if a[1] > b[1]:
+            return a
+        else:
+            return b
+
+    return reduce(tup_max, ((p, period_len_fraction(1, p)) for p in primes_upto(1000)))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+    print "Result: %s" % main()[0]
+
